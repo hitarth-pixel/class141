0a463e05b11b4f75d93f02697097cd5987fdbacd
hitarth-pixel <75882978+hitarth-pixel@users.noreply.github.com>
2022-08-15 17:21:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask,request,jsonify
+import csv
+
+all_movies=[]
+
+with open('movies.csv') as f:
+    reader = csv.reader(f)
+    
+    data=list(reader)
+    print(data)
+    all_movies = data[1:]
+
+liked_movies=[]
+not_liked_movies=[]
+did_not_watch=[]
+
+app = Flask(__name__)
+@app.route('/get-movie')
+def get_movie():
+    return jsonify({
+        'data':all_movies[0],
+        'status':'success'
+    }),201
+
+@app.route('/liked-movie',methods=['POST'])
+def liked_movie():
+    movie=all_movies[0]
+    all_movies=all_movies[1:]
+    liked_movies.append(movie)
+    return jsonify({
+        'status':'success'
+    }),201
+
+@app.route('/not-liked-movie',methods=['POST'])
+def not_liked_movie():
+    movie=all_movies[0]
+    all_movies=all_movies[1:]
+    not_liked_movies.append(movie)
+    return jsonify({
+        'status':'success'
+    }),201
+@app.route('/did_not_watch',methods=['POST'])
+def did_not_watch():
+    movie=all_movies[0]
+    all_movies=all_movies[1:]
+    did_not_watch.append(movie)
+    return jsonify({
+        'status':'success'
+    }),201
+
+if __name__=="__main__":
+    app.run()
